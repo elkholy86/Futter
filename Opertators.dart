c0b0f7635caa7c26c16175cca451674379103f24
elkholy86 <elkholy@windowslive.com>
2021-04-07 08:51:25 +0300

    7 - Opertators
    7 - Opertators

---
+++ Opertators.dart
@@ -0,0 +1,23 @@
+main()
+{
+  /*
+var x = 1 ;
+var y = 2 ;
+var z = x % y ;
+print ("z = $z");
+
+   */
+/*
+  var x = 9 ;
+  var y = 2 ;
+  var z = x % y ;
+  print ("z = $z");
+*/
+var x = 9 ;
+var y = 2 ;
+//x++ ;
+ x*=2 ;
+ y--;
+print(x is! int);
+  print(x > 5 || y < 7);
+}
