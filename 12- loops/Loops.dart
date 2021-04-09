42bca83185d1d92657aa7ed1bc54f2d1b7e3756d
elkholy86 <elkholy@windowslive.com>
2021-04-10 02:22:37 +0300

    12 loops
    aa

---
+++ 12- loops/Loops.dart
@@ -0,0 +1,48 @@
+main()
+{
+  /*
+  var x = 1 ;
+  while(x<=10) {
+    print("Hi");
+    x++;
+  }
+
+   */
+/*
+  var x = 1 ;
+  while (x<=10)
+    {
+      print ("4 * $x = ${4*x}");
+x++;
+    }
+
+ */
+/*
+  var x = 1 ;
+  do
+    {
+      print ("4 * $x = ${4*x}");
+      x++;
+    }
+  while (x<=10);
+
+ */
+/*
+for (var i = 1 ; i <=10 ; i++)
+  {
+    print ("4 * $i = ${4*i}");
+
+
+  }
+
+ */
+var x = {3 , 5.6 , true , "Ali"};
+  for (var i  in x)
+  {
+    print (i);
+
+
+  }
+
+
+}
