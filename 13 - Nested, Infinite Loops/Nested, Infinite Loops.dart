f15ff0c761f0019d0c797f3dad3f031b6aedece5
elkholy86 <elkholy@windowslive.com>
2021-04-10 13:37:30 +0300

    13 - Nested, Infinite Loops
    13 - Nested, Infinite Loops

---
+++ 13 - Nested, Infinite Loops/Nested, Infinite Loops.dart
@@ -0,0 +1,46 @@
+main() {
+  /*
+  for (var i = 1 ; i <= 10; i++)
+{
+  for (var j = 1; j <= 10; j++)
+    {
+      print ("$i*$j = ${i*j}");
+
+    }
+
+}
+
+   */
+  /*
+  var i = 1 ;
+  while (i <=10) {
+    var j = 1;
+    while (j < 10) {
+      print("$i*$j = ${i * j}");
+      j++;
+    }
+    i++;
+  }
+
+   */
+  /*
+  var i = 1 ;
+  do{
+    var j = 1;
+    do{
+      print("$i*$j = ${i * j}");
+      j++;
+    }
+    while (j <= 10);
+    i++;}
+  while (i <= 10);
+
+   */
+  var x = 1 ;
+  while (x<=10)
+    {
+      print("Hi");
+
+
+    };
+}
