5ff3e6eed0238e8930c96f5ecf2050b8f2419edd
elkholy86 <elkholy@windowslive.com>
2021-04-10 14:18:26 +0300

    14 - Control Statements
    14 - Control Statements

---
+++ 14 - Control Statements/14 - Control Statements.dart
@@ -0,0 +1,22 @@
+main ()
+{
+  for (var x = 1 ; x <=10 ; x++)
+    {
+      if (x==6) {break;}
+      print ("x = $x");
+
+    }
+  ali:
+  for (var i  = 1 ; i <=10 ;i++)
+    {
+
+      Mohammed :
+      for (var j  = 1 ; j <=10 ;j++)
+        {
+          if (j==4) {continue ali;}
+          print ("$i * $j = ${i*j}");
+
+        }
+
+    }
+}
