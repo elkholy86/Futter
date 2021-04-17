ade295d1fe89b4c7d9357442f45da244c6639345
elkholy86 <elkholy@windowslive.com>
2021-04-18 01:59:28 +0300

    20 - Lists
    20 - Lists

---
+++ 20 - Lists/Lists.dart
@@ -0,0 +1,49 @@
+main()
+{
+// 20 - Lists
+var list1=[4,5,6,7,8,9];
+print(list1.indexOf(7));
+/*
+list1.forEach((i){
+  print(i);
+ } );
+print(list1.length);
+for(var i = 0 ; i<list1.length;i++)
+{
+  print(list1[i]);
+}
+for(var i in list1)
+{
+  print(i);
+}
+list1.replaceRange(0, 2 ,[9,9]);
+print(list1);
+list1.removeRange(0, 2);
+print(list1);
+print(list1);
+print(list1[0]);
+print(list1[3]);
+print(list1.first);
+print(list1.last);
+print(list1.reversed.toList());
+list1[1]= 1 ;
+list1.add(10);
+print(list1);
+list1.addAll([3,7,8]);
+print(list1);
+list1.insert(0, 8);
+print(list1);
+list1.insertAll(0, [3,7,8]);
+print(list1);
+list1.remove(9);
+print(list1);
+list1.removeAt(0);
+print(list1);
+list1.removeLast();
+print(list1);
+
+
+
+ */
+
+}
