752f8c8548e60e9a51b498dfb9bed89f47f69fb2
elkholy86 <elkholy@windowslive.com>
2021-04-18 05:35:53 +0300

    22 - Sets
    22 - Sets

---
+++ 22 - Sets/22 - Sets.dart
@@ -0,0 +1,26 @@
+// 22 - Sets
+main ()
+{
+  var x = {4, 5, 6, 7, 8, 9};
+  x.forEach((item) {
+    print(item);
+  });
+  /*
+  Set y = {4, 5, 6, 7, 8, 9};
+  print (x);
+  print (y);
+  List z = x.toList();
+  print (z);
+  x.add(10);
+  print (x);
+  x.addAll({2,3});
+  print (x);  print (x.length);
+  print (x.elementAt(0));
+  print (x.contains(5));
+  print (x.remove(5));
+  print (x);
+  x.clear();
+  print (x);
+   */
+
+}
