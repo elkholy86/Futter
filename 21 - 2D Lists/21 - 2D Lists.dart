f83953449ff55c2e775354d084b208c81e1c8dba
elkholy86 <elkholy@windowslive.com>
2021-04-18 05:24:28 +0300

    21 - 2D Lists
    21 - 2D Lists

---
+++ 21 - 2D Lists/21 - 2D Lists.dart
@@ -0,0 +1,24 @@
+// 21 - 2D Lists
+main ()
+{
+
+var a = [[2,4,6] , [8,9,4]] ;
+for (var i in a )
+  {
+    print(i);
+  }
+
+//print (a[0][1]);
+  /*print (a);
+for (var i = 0 ; i<=1;i++)
+  {
+    for (var j = 0 ; j<3;j++)
+      {
+        print (a[i][j]);
+
+      }
+  }
+
+   */
+
+}
