0936833f9b40f1788be7187f5579931e61641bbc
elkholy86 <elkholy@windowslive.com>
2021-04-22 06:24:50 +0300

    23 - Maps
    23 - Maps

---
+++ 23 - Maps/23 - Maps.dart
@@ -0,0 +1,18 @@
+// 23 - Maps
+main ()
+{
+  var x =
+  {
+    9 : 'a' ,
+    8 : 'b',
+    7:'c',
+  };
+
+x.forEach((Keys, Values) {
+  print('Keys: $Keys , value: $Values');
+
+});
+//  print(x);
+
+
+}
