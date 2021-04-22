6c93af08b98ed3c30bf4cf2085d35882d7872cf8
elkholy86 <elkholy@windowslive.com>
2021-04-22 07:13:54 +0300

    24 - Fuctions
    24 - Fuctions

---
+++ 24 - Fuctions/24 - Fuctions.dart
@@ -0,0 +1,50 @@
+//24 - Fuctions
+void main()
+{
+  fun1();
+  print(fun2());
+  fun3(3 , 4);
+  print(fun4(3 , 4));
+  /*
+ // var x = 2 ;
+  //var v1 = info(2);
+  var v1 = sum(3,5);
+print(v1);
+
+   */
+}
+void fun1()
+{
+  print(3+4);
+}
+fun2()
+{
+  return(3+4);
+}
+void fun3(x,y)
+{
+  print(x+y);
+}
+fun4(x,y)
+{
+  return(x+y);
+}
+
+/*
+info (message)
+{
+  print ('message =  $message');
+}
+
+ */
+/*
+sum (x , y)
+{
+  return x+y;
+}
+
+ */
+sum (int x ,int  y)
+{
+  return x+y;
+}
