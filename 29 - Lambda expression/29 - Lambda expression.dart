e7e8f22d508669f130e8e9cd4e0313fc221f420c
elkholy86 <elkholy@windowslive.com>
2021-05-07 21:58:05 +0300

    29 - Lambda expression
    29 - Lambda expression

---
+++ 29 - Lambda expression/29 - Lambda expression.dart
@@ -0,0 +1,29 @@
+29 - Lambda expression
+main()
+{
+  a3(3,4);
+  /*
+  var x = [3 , 5 , 7 , 9];
+  x.forEach((i)=>print(i));
+
+   */
+  /*
+  lam1();
+  print(lam2());
+  lam3(5, 6);
+  print(lam4(6 , 4));
+
+   */
+}
+var al=()=>print(3+4);
+
+var a2 = ()=> 3+4;
+var a3 = (x ,y) =>print(x + y);
+var a4 = (x,y) => x+y;
+/*
+lam2()=> 3 +4 ;
+lam3(x , y)=> print(x+y);
+lam4(x , y)=> x+y;
+
+ */
+
