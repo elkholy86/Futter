728381fad7586db108407b6cf2a7341be6d71426
elkholy86 <elkholy@windowslive.com>
2021-05-25 01:27:33 +0300

    42 - Final vs Const
    42 - Final vs Const

---
+++ 42 - Final vs Const/42 - Final vs Const.dart
@@ -0,0 +1,25 @@
+// 42 - Final vs Const
+main ()
+{
+  final List a1 = ['a','s','l'];
+  final List a2 = ['a','s','l'];
+
+  a1.add('i');
+ // a2.add('e');
+  /*
+  final int a1 = DateTime.now().year;
+  const int a2 =2019 ;
+  var f = Final(5) ;
+  print(f.num1);
+
+   */
+}
+/*
+class Final
+{
+  final int num1 ;
+  static const int num2 = 7;
+  Final(this.num1);
+}
+
+ */
