201c6c4070311d90fe7de8194313887b3095c8bc
elkholy86 <elkholy@windowslive.com>
2021-06-23 12:19:14 +0300

    // 50 - Object Type Casting
    Problem with my code
     // 50 - Object Type Casting

---
+++ 50 - Object Type Casting/50 - Object Type Casting.dart
@@ -0,0 +1,32 @@
+ // 50 - Object Type Casting
+main ()
+{
+  var c = Casting <D> ();
+  var a = c.casting();
+a?.info();
+}
+
+ class Casting <T>
+{
+  T casting ()
+  {
+   T s;
+
+    return s;
+  }
+}
+abstract class X
+{}
+
+ abstract class Y
+ {}
+ abstract class C
+ {}
+ abstract class D {
+   info() {
+     print("Hi");
+   }
+ }
+  abstract class E
+  {
+  }
